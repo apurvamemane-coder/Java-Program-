fce25c20abaf48cd8e558262f8b8f0c26fd08c21
apurvamemane-coder <apurvamemane@gmail.com>
2025-09-20 16:34:48 +0530

    Add files via upload

---
+++ Simple if.java
@@ -0,0 +1,9 @@
+public class SimpleIfExample {
+    public static void main(String[] args) {
+        int number = 8;
+
+        if (number > 5) {
+            System.out.println("Number is greater than 5");
+        }
+    }
+}
