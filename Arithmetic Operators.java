78aae579d409e11244d7c65af48a06d793f09819
apurvamemane-coder <apurvamemane@gmail.com>
2025-09-20 17:55:44 +0530

    Add files via upload

---
+++ Arithmetic Operators.java
@@ -0,0 +1,12 @@
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        int a = 10, b = 5;
+
+        System.out.println("Arithmetic Operators:");
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+        System.out.println("a % b = " + (a % b));
+    }
+}
