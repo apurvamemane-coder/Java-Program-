d061b0400dd6982def316240f34ba60565e933ae
apurvamemane-coder <apurvamemane@gmail.com>
2025-09-20 18:05:54 +0530

    Add files via upload

---
+++ String.java
@@ -0,0 +1,10 @@
+public class SimpleString {
+    public static void main(String[] args) {
+        String name = "Alice";
+        System.out.println("Name: " + name);
+        System.out.println("Length: " + name.length());
+        System.out.println("Uppercase: " + name.toUpperCase());
+        System.out.println("Lowercase: " + name.toLowerCase());
+        System.out.println("First character: " + name.charAt(0));
+    }
+}
