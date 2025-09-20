03a702521f5421c4519ec140eb6f50f360413446
apurvamemane-coder <apurvamemane@gmail.com>
2025-09-20 16:08:17 +0530

    Add files via upload

---
+++ Nested if.java
@@ -0,0 +1,11 @@
+public class VerySimpleNestedIf {
+    public static void main(String[] args) {
+        int number = 5;
+
+        if (number > 0) {
+            if (number < 10) {
+                System.out.println("Number is between 1 and 9");
+            }
+        }
+    }
+}
