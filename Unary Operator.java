2bf7d174d62ef5417315b15f41ddc2c99b40358b
apurvamemane-coder <apurvamemane@gmail.com>
2025-09-20 17:57:52 +0530

    Add files via upload

---
+++ Unary Operator.java
@@ -0,0 +1,14 @@
+public class UnaryOperators {
+    public static void main(String[] args) {
+        int d = 5;
+
+        System.out.println("Unary Operators:");
+        System.out.println("Initial d: " + d);
+        System.out.println("++d (Pre-increment): " + (++d));
+        System.out.println("d++ (Post-increment): " + (d++));
+        System.out.println("After d++: " + d);
+        System.out.println("--d (Pre-decrement): " + (--d));
+        System.out.println("d-- (Post-decrement): " + (d--));
+        System.out.println("After d--: " + d);
+    }
+}
