d953ea3c02f600ceeb0688d3ce2f71e8d792e8da
apurvamemane-coder <apurvamemane@gmail.com>
2025-09-20 16:27:21 +0530

    Add files via upload

---
+++ if.java
@@ -0,0 +1,9 @@
+public class IfExample {
+    public static void main(String[] args) {
+        int number = 7;
+
+        if (number > 5) {
+            System.out.println("Number is greater than 5");
+        }
+    }
+}
