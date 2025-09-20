fbc2aa127dc343f2466262aab12e956c21c8bab9
apurvamemane-coder <apurvamemane@gmail.com>
2025-09-20 17:58:19 +0530

    Add files via upload

---
+++ Logical Operators.java
@@ -0,0 +1,10 @@
+public class LogicalOperators {
+    public static void main(String[] args) {
+        boolean x = true, y = false;
+
+        System.out.println("Logical Operators:");
+        System.out.println("x && y: " + (x && y));  // AND
+        System.out.println("x || y: " + (x || y));  // OR
+        System.out.println("!x: " + (!x));          // NOT
+    }
+}
