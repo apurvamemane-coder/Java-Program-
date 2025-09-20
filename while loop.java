b09db1b0eaa0b39e089f8b323b496e059b727764
apurvamemane-coder <apurvamemane@gmail.com>
2025-09-20 17:25:37 +0530

    Add files via upload

---
+++ while loop.java
@@ -0,0 +1,11 @@
+public class TableWhileLoop {
+    public static void main(String[] args) {
+        int i = 1;
+        int num = 3;
+
+        while (i <= 10) {
+            System.out.println(num + " x " + i + " = " + (num * i));
+            i++;
+        }
+    }
+}
